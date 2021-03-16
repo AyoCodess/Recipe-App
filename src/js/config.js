87cc02b88da96121b3a75715dc12c82a3697b64b
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-03-16 23:20:17 +0100

    Security Implementation API

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,14 @@
-'use strict';
+('use strict');
+
+export const getEnvVariable = (property, canBeUndefined = false) => {
+  const value = process.env[property];
+
+  if (!canBeUndefined && !value) {
+    throw new Error(`${property} environment variable is not set`);
+  }
+
+  return value;
+};
 
 // IMPORTANT this file is used to store all the varibles that are constants and will allow us to easily configure our project my ediiting the varibles in this file.
 
@@ -10,6 +20,6 @@ export const TIMEOUT_DURATION = 10;
 
 export const RES_PER_PAGE = 10;
 
-export const API_KEY = `dcb59068-88fa-415d-8a6e-10d983efe1ad`;
+export const API_KEY = getEnvVariable('RECIPES_API_KEY');
 
 export const MODAL_CLOSE_SEC = 2000;
